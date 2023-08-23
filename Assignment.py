743fc5ba182d72ae2b2148feaf61b6517f5eceb7
Nimisha Pithala <76213967+NimishaPithala@users.noreply.github.com>
2023-08-23 12:17:46 +0530

    CMT_MSG022

--- Assignment.py
+++ Assignment.py
@@ -2,94 +2,71 @@ print("Name: Aakanksha Pithala")
 print("EnRoll:E22CSEU1122")
 print("Batch: 38")
 class Flight:
-    def __init__(self, flight_id, source, destination, price):
+    def __init__(self, flight_id, origin, destination, price):
         self.flight_id = flight_id
-        self.source = source
+        self.origin = origin
         self.destination = destination
         self.price = price
 
-class FlightDatabase:
-    def __init__(self):
-        self.flights = []
+class FlightTable:
+    def __init__(self, flights):
+        self.flights = flights
 
-    def add_flight(self, flight):
-        self.flights.append(flight)
-
-    def search_flights_by_city(self, city):
+    def search_city(self, city):
         result = []
         for flight in self.flights:
-            if city.lower() in (flight.source.lower(), flight.destination.lower()):
+            if flight.origin == city or flight.destination == city:
                 result.append(flight)
         return result
 
-    def search_flights_from_city(self, city):
+    def search_from_city(self, city):
         result = []
         for flight in self.flights:
-            if city.lower() == flight.source.lower():
+            if flight.origin == city:
                 result.append(flight)
         return result
 
-    def search_flights_between_cities(self, source, destination):
+    def search_between_cities(self, origin, destination):
         result = []
         for flight in self.flights:
-            if source.lower() == flight.source.lower() and destination.lower() == flight.destination.lower():
+            if flight.origin == origin and flight.destination == destination:
                 result.append(flight)
         return result
 
-def main():
-    database = FlightDatabase()
+flights = [
+    Flight("AI161E90", "BLR", "BOM", 5600),
+    Flight("BR161F91", "BOM", "BBI", 6750),
+    Flight("AI161F99", "BBI", "BLR", 8210),
+    Flight("VS171E20", "JLR", "BBI", 5500),
+    Flight("AS171G30", "HYD", "JLR", 4400),
+    Flight("AI131F49", "HYD", "BOM", 3499)
+]
 
-    # Adding flights to the database
-    database.add_flight(Flight("AI161E90", "BLR", "BOM", 5600))
-    database.add_flight(Flight("BR161F91", "BOM", "BBI", 6750))
-    database.add_flight(Flight("AI161F99", "BBI", "BLR", 8210))
-    database.add_flight(Flight("VS171E20", "JLR", "BBI", 5500))
-    database.add_flight(Flight("AS171G30", "HYD", "JLR", 4400))
-    database.add_flight(Flight("AI131F49", "HYD", "BOM", 3499))
+flight_table = FlightTable(flights)
 
-    while True:
-        print("Search Options:")
-        print("1. Flights for a particular City")
-        print("2. Flights From a city")
-        print("3. Flights between two given cities")
-        print("4. Exit")
+print("please choose the kind of search you want to perform")
+print("1. search for Flights for a particular City")
+print("2. search for Flights From a city")
+print("3. search for Flights between two given cities")
+print("4. exit")
+user_ch = int(input("Enter your choice: "))
+while user_ch != 4:
+    if user_ch == 1:
+        city = input("Enter the city: ")
+        result = flight_table.search_city(city)
+    elif user_ch == 2:
+        city = input("Enter the city: ")
+        result = flight_table.search_from_city(city)
+    elif user_ch == 3:
+        origin = input("Enter the origin city: ")
+        destination = input("Enter the destination city: ")
+        result = flight_table.search_between_cities(origin, destination)
+    elif user_ch == 4:
+        break
+    else:
+        print("Invalid user_ch")
+        result = []
 
-        choice = int(input("Enter your choice: "))
-        
-        if choice == 1:
-            city = input("Enter the city name: ")
-            result = database.search_flights_by_city(city)
-            if result:
-                for flight in result:
-                    print(f"Flight ID: {flight.flight_id}, Source: {flight.source}, Destination: {flight.destination}, Price: {flight.price}")
-            else:
-                print("No flights found for the specified city.")
-        
-        elif choice == 2:
-            city = input("Enter the source city name: ")
-            result = database.search_flights_from_city(city)
-            if result:
-                for flight in result:
-                    print(f"Flight ID: {flight.flight_id}, Source: {flight.source}, Destination: {flight.destination}, Price: {flight.price}")
-            else:
-                print("No flights found from the specified city.")
-        
-        elif choice == 3:
-            source = input("Enter the source city name: ")
-            destination = input("Enter the destination city name: ")
-            result = database.search_flights_between_cities(source, destination)
-            if result:
-                for flight in result:
-                    print(f"Flight ID: {flight.flight_id}, Source: {flight.source}, Destination: {flight.destination}, Price: {flight.price}")
-            else:
-                print("No flights found between the specified cities.")
-        
-        elif choice == 4:
-            print("Exiting the program.")
-            break
-        
-        else:
-            print("Invalid choice. Please select a valid option.")
-
-if __name__ == "__main__":
-    main()
+if user_ch != 4:
+    for flight in result:
+        print(flight.flight_id, flight.origin, flight.destination, flight.price)
